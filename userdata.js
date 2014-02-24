5586794ab564882a28ea8afa31cfb516339597c9
Jack Henley <jack.d.henley@gmail.com>
2014-02-24 16:42:52 -0500

    User name validation working

--- userdata.js
+++ userdata.js
@@ -1,15 +1,56 @@
 $(document).ready(function(){
+  var name,
+    age,
+    phone,
+    email,
+    name_error,
+    age_error,
+    phone_error,
+    email_error;
 
-}
+  $('span').hide();
+  name_error = $('#user_name_error');
+  age_error = $('#user_age_error');
+  phone_error = $('#user_phone_error');
+  email_error = $('#user_email_error');
+
+
+
+  $('#add_user_data').submit(function(event) {
+    event.preventDefault();
+    user_name = $('#user_name').val();
+    user_age = $('#user_age').val();
+    user_ph = $('#user_ph').val();
+    user_email = $('#user_email').val();
+
+    ValidationApp.validateName(user_name);
+    // ValidationApp.validatePhone(user_ph);
+    // ValidationApp.validateEmail(user_email);
+
+    // ValidationApp.validateage(user_age);
+  })
+
+
+});
 
 var ValidationApp = {
+
+};
+
+ValidationApp.validateName = function(name) {
+  if(name) {
+    alert('yayyy!');
+  }
+  else {
+    alert('nooooo!');
+  }
 };
 
 ValidationApp.validatePhone = function() {
   //This is a regular expression for a phone
   var phoneRegex = /^[0-9]+[0-9]+[0-9]+[_.-]+[0-9]+[0-9]+[0-9]+[_.-]+[0-9]+[0-9]+[0-9]+[0-9]$/;
 
-  if(phoneMatch.test(this.userPhoneVal)) {
+  if(phoneRegex.test(this.userPhoneVal)) {
   }
   else {
   }
